813423418399b00bc90d9c10205fc46815cf5011
tse2003 <s.tseba0124@gmail.com>
2024-10-27 16:46:30 +0800

    front-change

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -4,8 +4,14 @@ export default function Home() {
   return (
     <div className="m-auto w-[1000px] h-full">
       <h1 className="text-white text-center font-bold text-4xl pb-5">Хамгийн их үзэлттэй кино</h1>
-      <div className="m-auto w-[1000px] h-[400px] bg-gray-200 relative">
-        <Image src="/Zone.jpg" layout="fill" objectFit="cover" alt="logo"/>
+      <div className="m-auto w-[1000px] h-[400px] bg-gray-200 relative group">
+        <Image src="/Zone.jpg" layout="fill" objectFit="cover" alt="logo" />
+        <div className="absolute inset-0 bg-black bg-opacity-30 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+          <h2 className="text-white text-lg p-5">
+            <h1 className="text-center text-3xl font-bold">Z zone</h1>
+            Бөглүү ойн дунд нэгэн нууцлаг цэргийн баззад гэмт хэрэгтнүүд ирнэ. Тэд эхэндээ зүгээр л хатуу дэглэмтэй хөдөлмөрийн лагер гэж бодож байсан болов ч үгүй байв. Хэрэг буруугаар эргэж нууцлаг Зомбины халдвар тархаж эхэллээ. Тэд амьд үлдэж чадах болов уу?
+          </h2>
+        </div>
       </div>
       <h1 className="text-white font-bold text-4xl pt-5">Дэлгэцээр гарч буй кинонууд</h1>
       <div className="carousel rounded-box mt-5">
