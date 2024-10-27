2d3f476384bc53a1fc8a16073a14f42ec20603c9
tse2003 <s.tseba0124@gmail.com>
2024-10-27 11:44:04 +0800

    Success - edit

--- admin-panel/app/api/products/route.js
+++ admin-panel/app/api/products/route.js
@@ -2,9 +2,15 @@ import connectMongoDB from "@/libs/mongodb";
 import Product from "@/models/ProductModel";
 import { NextResponse } from "next/server";
 
+export async function GET() {
+    await connectMongoDB();
+    const products = await Product.find();
+    return NextResponse.json({ products });
+}
+
 export async function POST(request) {
     const { name, image, price, category } = await request.json();
     await connectMongoDB();
     await Product.create({name, image, price, category});
-    return NextResponse.json({ message: "Product Created" }, { status: 201 })
+    return NextResponse.json({ message: "Product Created" }, { status: 201 });
 }
--- admin-panel/components/ProductsList.js
+++ admin-panel/components/ProductsList.js
@@ -1,6 +1,22 @@
+import Image from "next/image";
 import Link from "next/link";
 
+const getProducts = async () => {
+    try {
+        const res = await fetch("http://localhost:3000/api/products", {
+            cache: "no-store",
+        });
+        if(!res.ok) {
+            throw new Error("Failed to fetch products");
+        }
+        return res.json();
+    } catch (error) {
+        console.log("Error loading products: ", error)
+    }
+};
+
 export default async function ProductsList() {
+    const { products } = await getProducts();
     return (
         <>
             <div className="overflow-x-auto">
@@ -12,6 +28,60 @@ export default async function ProductsList() {
                         Add Product
                     </Link>
                 </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Name</th>
+                            <th>Price</th>
+                            <th>Category</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((rs) => (
+                            <tr className="hover" key={rs._id}>
+                                <th>
+                                    <label>
+                                        <input type="checkbox" className="checkbox" />
+                                    </label>
+                                </th>
+                                <td>
+                                    <div className="flex items-center gap-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <Image 
+                                                    src={rs.image}
+                                                    alt={rs.name}
+                                                    width={80}
+                                                    height={80}
+                                                    className="rounded-lg"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{rs.name}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    ${rs.price}
+                                </td>
+                                <td>{rs.category}</td>
+                                <th>
+                                    <Link href={`/editProduct/${rs._id}`}>
+                                        <button className="btn btn-primary">Edit</button>
+                                    </Link>
+                                    Delete
+                                </th>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </>
     );
